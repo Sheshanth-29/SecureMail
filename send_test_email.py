74332badeedc6d0abb1937a54dd8bd9b563289a8
Sheshanth-29 <sheshanthsaravanan@gmail.com>
2026-09-05 14:38:26 +0530

    remove credentials

--- send_test_email.py
+++ send_test_email.py
@@ -1,17 +1,18 @@
-
+import os
 import smtplib
 
 sender = "Private Person <from@example.com>"
 receiver = "A Test User <to@example.com>"
-
 message = f"""\
 Subject: Hi Mailtrap
 To: {receiver}
 From: {sender}
-
 This is a test e-mail message."""
+
+SMTP_USER = os.environ.get("MAILTRAP_USER")
+SMTP_PASS = os.environ.get("MAILTRAP_PASS")
 
 with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
     server.starttls()
-    server.login("0ce8db7be5ed1a", "6e584daf350939")
+    server.login(SMTP_USER, SMTP_PASS)
     server.sendmail(sender, receiver, message)
